19b6efa1b907da23cf6c08ac3de41f1654fbbac8
Mateusz <matstepien@outlook.com>
2017-06-28 13:49:59 +0200

    country search

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-var url = 'https://restcountries.eu/rest/v1/name/';
+var url = 'https://restcountries.eu/rest/v2/name/';
 	countriesList = $('#countries');
 
 $('#search').click(searchCountries);
 
-function searchCountries() {
-  	var countryName = $('#country-name').val();
-}
 
 function searchCountries() {
  	var countryName = $('#country-name').val();
-if(!countryName.length) countryName = 'Poland';
-$.ajax({
-  		url: url + countryName,
-  		method: 'GET',
-  		success: showCountriesList
-  	});
+	if(!countryName.length) countryName = 'Poland';
+	$.ajax({
+		url: url + countryName,
+		method: 'GET',
+		success: showCountriesList
+	});
 }
 function showCountriesList(resp) {
-  countriesList.empty();
-resp.forEach(function(item) {
+	countriesList.empty();
+	resp.forEach(function(item) {
 		$('<li>').text(item.name).appendTo(countriesList);
 });
 }
